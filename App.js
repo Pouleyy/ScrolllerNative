aa3c0ed5c2dd3c622c3986a4540f202d56b42b9b
Kevin Harismendy <kevin.harismendy@outlook.com>
2018-11-17 23:04:56 +0100

    :sparkles: PersistGate seems as good as Stargate

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { StatusBar } from 'react-native';
 
 import StackNavigator from './app/config/routes';
+import { persistor, store } from './app/config/store';
+
+import { PersistGate } from 'redux-persist/lib/integration/react';
+
+import { Text } from 'react-native';
 
 export default class App extends React.Component {
-  componentDidMount() {}
-
   render() {
-    return <StackNavigator />;
+    // Do a function to purge every 24 or 48h
+    // persistor.purge()
+    return (
+      <Provider store={store}>
+        <PersistGate loading={<Text>Loading</Text>} persistor={persistor}>
+          <StatusBar barStyle="dark-content" />
+          <StackNavigator />
+        </PersistGate>
+      </Provider>
+    );
   }
 }
