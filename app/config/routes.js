1624785c8074a55bb8598d67e5360d54b8433927
Yann AVONDO <yann.avondo@gmail.com>
2018-11-18 18:19:06 +0100

    :lipstick: bottom menu things

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,4 +1,6 @@
-import { StackNavigator, createBottomTabNavigator } from 'react-navigation';
+import { StackNavigator, createBottomTabNavigator, BottomTabBar } from 'react-navigation';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 
 import Home from '../screens/home';
 
@@ -7,11 +9,20 @@ const MainNavigator = createBottomTabNavigator(
     home: Home
   },
   {
-    initialRouteName: 'home'
-    // tabBarOptions: {
-    //   activeBackgroundColor: '#1b2e2e'
-    // }
+    initialRouteName: 'home',
+    tabBarOptions: {
+      activeTintColor: '#f2f2f2',
+      activeBackgroundColor: '#2EC4B6',
+      inactiveTintColor: '#666',
+      labelStyle: {
+        fontSize: 22,
+        textAlign: "center",
+        marginBottom: 10
+      },
+      tabBarIcon: ({ focused }) => {
+        return <Icon name='home' size={25} />;
+      }
+    }
   }
 );
-
 export default MainNavigator;
