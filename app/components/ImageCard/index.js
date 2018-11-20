2723e69907d83224050dc1f3b5d543989eee6798
Kevin Harismendy <kevin.harismendy@outlook.com>
2018-11-20 18:11:04 +0100

    :art: Add ImageCard component

---
+++ app/components/ImageCard/index.js
@@ -0,0 +1,48 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { ImageBackground, View, Image } from 'react-native';
+
+import styled from 'styled-components';
+import Heart from '../../static/images/heart.png';
+
+const LikeTouchableOpacity = styled.TouchableOpacity``;
+
+export default class ImageCard extends React.PureComponent {
+  static propTypes = {
+    navigation: PropTypes.object,
+    media: PropTypes.object
+  };
+
+  render() {
+    const { media } = this.props;
+
+    return (
+      <ImageBackground
+        source={{ uri: media.url }}
+        style={{ width: '100%', height: 300 }}
+        resizeMode={'stretch'}
+      >
+        <View
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            justifyContent: 'flex-end',
+            alignItems: 'flex-end'
+          }}
+        >
+          <LikeTouchableOpacity
+          //onPress={() => this.handleFavButtonPress(media)}
+          >
+            <Image
+              source={Heart}
+              style={{ width: 25, height: 25, tintColor: 'red' }}
+            />
+          </LikeTouchableOpacity>
+        </View>
+      </ImageBackground>
+    );
+  }
+}
--- app/components/MediaCard/index.js
+++ app/components/MediaCard/index.js
@@ -1,10 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import VideoCard from '../VideoCard';
-
-import styled from 'styled-components';
-const LikeTouchableOpacity = styled.TouchableOpacity``;
-import Heart from '../../static/images/heart.png';
+import ImageCard from '../ImageCard';
 
 export default class MediaCard extends React.PureComponent {
   static propTypes = {
@@ -18,34 +15,7 @@ export default class MediaCard extends React.PureComponent {
     if (media.url.endsWith('.webm') || media.url.endsWith('.mp4')) {
       element = <VideoCard media={media} />;
     } else {
-      element = (
-        <ImageBackground
-          source={{ uri: media.url }}
-          style={{ width: '100%', height: 300 }}
-          resizeMode={'stretch'}
-        >
-          <View
-            style={{
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              right: 0,
-              bottom: 0,
-              justifyContent: 'flex-end',
-              alignItems: 'flex-end'
-            }}
-          >
-            <LikeTouchableOpacity
-            //onPress={() => this.handleFavButtonPress(media)}
-            >
-              <Image
-                source={Heart}
-                style={{ width: 25, height: 25, tintColor: 'red' }}
-              />
-            </LikeTouchableOpacity>
-          </View>
-        </ImageBackground>
-      );
+      element = <ImageCard media={media} />;
     }
     return element;
   }
